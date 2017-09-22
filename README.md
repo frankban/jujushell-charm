d7705ac67545d1f15542dc700dfb1b8938cb560b
Madison Scott-Clary <madison.scott-clary@canonical.com>
2017-09-21 18:24:32 -0600

    Working charm

--- README.md
+++ README.md
@@ -10,11 +10,11 @@ This charm sets the following states:
 
 ## Current status
 
-As of 2017-09-12, this charm simply installs juju on a fresh xenial system. This will require the user to run `juju register`/`juju login` once they have access to the terminal.
+* As of 2017-09-21, this charm installs terminado and starts it listening, along with a webpage running xterm.js with minimal styling
+* As of 2017-09-12, this charm simply installs juju on a fresh xenial system. This will require the user to run `juju register`/`juju login` once they have access to the terminal.
 
 The next tasks are:
 
-* Install terminado and start it listening, along with a webpage running xterm.js with minimal styling
 * Install and set up LXD, and have the terminado server run on a prepared LXD image with a simple WebSocket proxy to direct the user's commands to that
 * Have the GUI recognize when this charm is added to the environment and have the xterm.js portion embedded in the GUI
 * Pass macaroons to the charm so that the user does not need to register/login
--- files/juju-shell/juju-shell.py
+++ files/juju-shell/juju-shell.py
@@ -1,31 +1,50 @@
 """A single common terminal for all websockets.
 """
-import tornado.web
+import os
+from tornado import (
+    ioloop,
+    web,
+)
 # This demo requires tornado_xstatic and XStatic-term.js
 import tornado_xstatic
 
+import terminado
 from terminado import TermSocket, SingleTermManager
-from common_demo_stuff import run_and_show_browser, STATIC_DIR, TEMPLATE_DIR
 
-class TerminalPageHandler(tornado.web.RequestHandler):
+
+STATIC_DIR = os.path.join(os.path.dirname(terminado.__file__), "_static")
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), "templates")
+
+
+class TerminalPageHandler(web.RequestHandler):
     def get(self):
         return self.render("termpage.html", static=self.static_url,
                            xstatic=self.application.settings['xstatic_url'],
                            ws_url_path="/websocket")
 
+
 def main(argv):
-    term_manager = SingleTermManager(shell_command=['bash'])
+    term_manager = terminado.SingleTermManager(shell_command=['bash'])
     handlers = [
-                (r"/websocket", TermSocket,
+                (r"/websocket", terminado.TermSocket,
                      {'term_manager': term_manager}),
                 (r"/", TerminalPageHandler),
                 (r"/xstatic/(.*)", tornado_xstatic.XStaticFileHandler,
                      {'allowed_modules': ['termjs']})
                ]
-    app = tornado.web.Application(handlers, static_path=STATIC_DIR,
+    app = web.Application(handlers, static_path=STATIC_DIR,
                       template_path=TEMPLATE_DIR,
                       xstatic_url = tornado_xstatic.url_maker('/xstatic/'))
     app.listen(8765, '0.0.0.0')
+    loop = ioloop.IOLoop.instance()
+    try:
+        loop.start()
+    except KeyboardInterrupt:
+        print(" Shutting down on SIGINT")
+    finally:
+        term_manager.shutdown()
+        loop.close()
+
 
 if __name__ == '__main__':
     main([])
