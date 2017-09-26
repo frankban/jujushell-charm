b2396a4dc9025c2a49a2e17187937916f2b8ae87
Madison Scott-Clary <madison.scott-clary@canonical.com>
2017-09-26 13:54:58 -0400

    Use jujushell instead of terminado directly

--- reactive/juju-shell.py
+++ reactive/juju-shell.py
@@ -2,12 +2,13 @@ import os
 import subprocess
 
 from charmhelpers.core.hookenv import (
-open_port,
-status_set,
+    open_port,
+    status_set,
 )
 from charmhelpers.core.templating import render
 from charms.reactive import (
     hook,
+    remove_state,
     set_state,
 )
 
@@ -17,15 +18,25 @@ FILES = os.path.join(os.getcwd(), 'files')
 
 @hook('install')
 def install_service():
-    """Installs the juju-shell systemd service."""
-    render('juju-shell.service',
-           '/usr/lib/systemd/user/juju-shell.service',
-           {'juju_shell': os.path.join(FILES, 'juju-shell', 'juju-shell.py')},
-           perms=664)
+    """Installs the jujushell systemd service."""
+    render('jujushell.service',
+           '/usr/lib/systemd/user/jujushell.service',
+           # {'juju_shell': os.path.join(FILES, 'juju-shell', 'juju-shell.py')},
+           {
+               'jujushell': os.path.join(FILES, 'jujushell'),
+               'jujushell_config': os.path.join(FILES, 'config.yaml'),
+           },
+           perms=775)
+    api_addrs = os.environ.get('JUJU_API_ADDRESSES')
+    if api_addrs is None:
+        raise "Could not find API addresses"
+    api_addrs = str(api_addrs.split())
+    render('config.yaml', os.path.join(FILES, 'config.yaml'),
+           {'api_addrs': api_addrs})
     subprocess.check_call(('systemctl', 'enable',
-                           '/usr/lib/systemd/user/juju-shell.service'))
+                           '/usr/lib/systemd/user/jujushell.service'))
     subprocess.check_call(('systemctl', 'daemon-reload'))
-    set_state('juju-shell.installed')
+    set_state('jujushell.installed')
 
 
 @hook('start')
@@ -36,18 +47,23 @@ def install_deps():
     # For now, open the port on which terminado is running. This is 8765 instead
     # of 80 so that the service can be run as ubuntu, rather than root. This
     # Will be changed when we run the WS proxy.
-    open_port(8765)
+    open_port(8047)
     restart()
 
 
 @hook('start')
 @hook('config-changed')
 def restart():
-    """Restarts the juju-shell service."""
-    subprocess.check_call(('systemctl', 'restart', 'juju-shell.service'))
+    """Restarts the jujushell service."""
+    subprocess.check_call(('systemctl', 'restart', 'jujushell.service'))
+    status_set('active', 'jujushell started')
+    set_state('jujushell.started')
+    remove_state('jujushell.stopped')
 
 
 @hook('stop')
 def stop():
-    """Stops the juju-shell service."""
-    subprocess.check_call(('systemctl', 'stop', 'juju-shell.service'))
+    """Stops the jujushell service."""
+    subprocess.check_call(('systemctl', 'stop', 'jujushell.service'))
+    remove_state('jujushell.started')
+    set_state('jujushell.stopped')
