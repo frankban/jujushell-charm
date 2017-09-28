ec9d58a912d565fc1e308e0900b67687a1ed3d93
Madison Scott-Clary <madison.scott-clary@canonical.com>
2017-09-28 08:38:20 -0400

    charm starts up

--- reactive/juju-shell.py
+++ reactive/juju-shell.py
@@ -28,33 +28,57 @@ def build_config():
     cfg = config()
     api_addrs = os.environ.get('JUJU_API_ADDRESSES')
     if api_addrs is None:
-        raise ValueError("Could not find API addresses")
+        raise ValueError('Could not find API addresses')
     api_addrs = api_addrs.split()
-    render('config.yaml', os.path.join(FILES, 'config.yaml'),
-           {
-               'api_addrs': yaml.safe_dump(api_addrs),
-               'image_name': IMAGE_NAME,
-               'port': cfg['port'],
-           })
+    render('config.yaml', os.path.join(FILES, 'config.yaml'), {
+       'api_addrs': yaml.safe_dump(api_addrs),
+       'image_name': IMAGE_NAME,
+       'port': cfg['port'],
+       'log_level': cfg['log-level']
+   })
+
+
+def manage_ports():
+   """Opens the port on which to listen, closing the previous if needed."""
+   cfg = config()
+   if cfg.changed('port'):
+       log('port updated from {} to {}'.format(
+       cfg.previous('port'), cfg['port']))
+       close_port(cfg.previous('port'))
+   open_port(cfg['port'])
+   build_config()
+
 
 @hook('install')
 def install_service():
     """Installs the jujushell systemd service."""
     # Render the jujushell systemd service module.
-    log('creating systemd module')
+    status_set('maintenance', 'creating systemd module')
     render('jujushell.service',
-           '/usr/lib/systemd/user/jujushell.service',
-           {
+           '/usr/lib/systemd/user/jujushell.service', {
                'jujushell': os.path.join(FILES, 'jujushell'),
                'jujushell_config': os.path.join(FILES, 'config.yaml'),
            },
            perms=775)
 
-    # Retrive the lxc image from the the resource and import it.
-    log('fetching lxc image')
+    # Retrieve the jujushell binary resource.
+    resource = resource_get('jujushell')
+    if not resource:
+        raise ValueError('Could not retrieve jujushell resource')
+    os.rename(resource, os.path.join(FILES, 'jujushell'))
+    os.chmod(os.path.join(FILES, 'jujushell'), 0o775)
+
+    # Retrieve the LXD image from the the resource and import it.
+    status_set('maintenance', 'fetching LXD image')
+    subprocess.check_call(('lxd', 'init', '--auto'))
     resource = resource_get('termserver')
     if not resource:
-        raise ValueError("Could not retrieve termserver resource")
+        raise ValueError('Could not retrieve termserver resource')
+    try:
+        # Catch an exception here in case we are retrying this hook.
+        subprocess.check_call(('lxc', 'image', 'delete', IMAGE_NAME))
+    except:
+        log('image does not yet exist')
     subprocess.check_call(('lxc', 'image', 'import', resource,
                            '--alias={}'.format(IMAGE_NAME)))
 
@@ -62,34 +86,34 @@ def install_service():
     build_config()
 
     # Enable the jujushell module.
-    log('enabling systemd module')
+    status_set('maintenance', 'enabling systemd module')
     subprocess.check_call(('systemctl', 'enable',
                            '/usr/lib/systemd/user/jujushell.service'))
     subprocess.check_call(('systemctl', 'daemon-reload'))
     set_state('jujushell.installed')
+    status_set('maintenance', 'jujushell installed')
 
 
 @hook('start')
+def start():
+    restart()
+
+
 @hook('config-changed')
+def config_changed():
+    restart()
+
+
 def restart():
     """Restarts the jujushell service."""
-    log('(re)starting the jujushell service')
+    log('BOOM WE PROBABLY HIT START')
+    status_set('maintenance', '(re)starting the jujushell service')
+    manage_ports()
     subprocess.check_call(('systemctl', 'restart', 'jujushell.service'))
     status_set('active', 'jujushell started')
     set_state('jujushell.started')
     remove_state('jujushell.stopped')
 
-
-@hook('config-changed')
-def manage_ports():
-    """Opens the port on which to listen, closing the previous if needed."""
-    cfg = config()
-    if cfg.change('port'):
-        log('port updated from {} to {}'.format(
-            cfg.previous('port'), cfg['port']))
-        close_port(cfg.previous('port'))
-    open_port(cfg['port'])
-    build_config()
 
 
 @hook('stop')
